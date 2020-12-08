b40eb4096a482c78252f87d513d5ee8db4943bb3
Vaintti <Vaintti@users.noreply.github.com>
2020-12-08 18:29:35 +0200

    Add day 2 task 1

--- AdventOfCode/AdventOfCode.cpp
+++ AdventOfCode/AdventOfCode.cpp
@@ -5,12 +5,14 @@ using namespace std;
 #include <iostream>
 #include <list>
 #include "day1.h"
-
+#include "day2.h"
 
 int main()
 {
-	cout << "Day 1 task 1\n";
-	day1Task1();
-	cout << "\nDay 1 task 2\n";
-	day1Task2();
+	//cout << "Day 1 task 1\n";
+	//day1Task1();
+	//cout << "\nDay 1 task 2\n";
+	//day1Task2();
+	cout << "\nDay 2 task 1\n";
+	day2Task1();
 }
---
+++ AdventOfCode/day2.cpp
@@ -0,0 +1,39 @@
+using namespace std;
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+
+void day2Task1() {
+	int validCount = 0;
+	ifstream in("../Debug/input_d2_t1.txt");
+	string str;
+	while (getline(in, str)) {
+		if (str.size() <= 0) continue;
+		int splitIndex = str.find(": ");
+		string rule = str.substr(0, splitIndex);
+		string password = str.substr(splitIndex + 2, str.length() - (splitIndex + 2));
+
+		splitIndex = rule.find(" ");
+		string range = rule.substr(0, splitIndex);
+		char letter = rule.at(splitIndex + 1);
+
+		splitIndex = range.find("-");
+		int min = stoi(range.substr(0, splitIndex));
+		int max = stoi(range.substr(splitIndex + 1, range.length() - (splitIndex + 1)));
+
+		int lettersInPassword = 0;
+
+		for (char& c : password) {
+			if (c == letter) lettersInPassword++;
+		}
+
+		if (lettersInPassword < min || lettersInPassword > max) continue;
+
+		validCount++;
+	}
+	in.close();
+
+	cout << validCount;
+}
